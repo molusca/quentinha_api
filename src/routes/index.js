f1db0262c1d059e5cc519fcefc0314132ebdbbdf
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-05-22 08:55:08 -0300

    restaurant crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const loginRouter = require('./login');
 const userRouter = require('./user');
 const departmentRouter = require('./department');
+const restaurantRouter = require('./restaurant');
 
 router.get('/', function (req, res) {
   res.json({success: true, message: 'Hello World!'});
@@ -11,5 +12,6 @@ router.get('/', function (req, res) {
 router.use('/login', loginRouter);
 router.use('/user', userRouter);
 router.use('/department', departmentRouter);
+router.use('/restaurant', restaurantRouter);
 
 module.exports = router;
